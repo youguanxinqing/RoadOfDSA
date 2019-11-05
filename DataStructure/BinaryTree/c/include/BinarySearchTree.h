1eff62d426b1abef7f49c4cf312b5e2c9ab9eccd
youguanxinqing <youguanxinqing@qq.com>
2019-11-05 22:58:11 +0800

    binary search tree 019.11.5 22:58

--- DataStructure/BinaryTree/c/include/BinarySearchTree.h
+++ DataStructure/BinaryTree/c/include/BinarySearchTree.h
@@ -1,25 +1,22 @@
 #ifndef __BINART_SEARCH_TREE__
 #define __BINART_SEARCH_TREE__
 
-#include <stdio.h>
-#include <stdlib.h>
-
 struct TreeNode;
-typedef struct TreeNode *Position;
-typedef struct TreeNode *SearchTree;
+typedef TreeNode *Position;
+typedef TreeNode *SearchTree;
 typedef int Element;
 
-SearchTree MakeEmpty(SearchTree T);
-Position Find(Element E, SearchTree T);
-Position FindMin(SearchTree T);
-Position FindMax(SearchTree T);
-SearchTree Insert(Element E, SearchTree T);
-SearchTree Delete(Element E, SearchTree T);
-Element Retrive(Position T);
+SearchTree MakeEmpty( SearchTree T );
+Position Find( Element X, SearchTree T );
+Position FindMin( Element X, SearchTree T );
+Position FindMax( Element X, SearchTree T );
+SearchTree Insert( Element X, SearchTree T );
+SearchTree Delete( Element X, SearchTree T );
+Element Retrieve( Position P );
 
-#endif
+#endif 
 
 struct TreeNode {
-    Element E;
+    Element val;
     SearchTree Left, Right;
 };
