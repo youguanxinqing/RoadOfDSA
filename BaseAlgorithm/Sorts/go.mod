6264502069bdca357fd7291b23a2a5f9c6d3ece3
youguanxinqing <youguanxinqing@qq.com>
2020-02-19 00:07:23 +0800

    change package path

--- BaseAlgorithm/Sorts/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/youguanxinqing/RoadOfDSA/BaseAlgorithm/Sorts
-
-go 1.13
--- gohelper/sorts/quick_sort_test.go
+++ gohelper/sorts/quick_sort_test.go
@@ -2,13 +2,11 @@ package sorts
 
 import (
 	"testing"
-
-	sorts "github.com/youguanxinqing/RoadOfDSA/BaseAlgorithm/Sorts"
 )
 
 func TestQuickSort(t *testing.T) {
 	nums := []int{6, 3, 9, 10, 2, 1, 1, 2}
-	sorts.QuickSort(nums)
+	QuickSort(nums)
 
 	length := len(nums) - 1
 	for i := 1; i <= length; i++ {
