ca327f8a0a7d4af26e06d06a94edcce54be392c0
youguanxinqing <youguanxinqing@qq.com>
2020-02-19 00:49:58 +0800

    leetcode 455

--- LeetCode/455/assign_cookies.go
+++ LeetCode/455/assign_cookies.go
@@ -1,21 +1,22 @@
 package assign_cookies
 
-import "leetcode/tools/sorts"
+import "sort"
 
 func findContentChildren(g, s []int) int {
-	sorts.QuickSort(g)
-	sorts.QuickSort(s)
+	// 使用官方排序包，用自己的排序函数会导致时间超时
+	sort.Ints(g)
+	sort.Ints(s)
 
 	count := 0
-	slen := len(s) - 1
-	for _, gi := range g {
-		if slen < count {
+	glen := len(g) - 1
+	for _, si := range s {
+		if count > glen {
 			break
 		}
-		if gi > s[count] {
-			continue
+		// 从最小胃口开始满足(g[0] 最小胃口)
+		if si >= g[count] {
+			count++
 		}
-		count++
 	}
 	return count
 }
